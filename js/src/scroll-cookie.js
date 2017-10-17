0016e8baecd69c20ece0eeaa636620ce40b0f60d
gengjuanjuan <344363732@qq.com>
2017-10-17 19:05:59 +0800

    Site updated: 2017-10-17 19:05:58

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Oct 17 2017 14:09:47 GMT+0800 (CST)
+// build time:Tue Oct 17 2017 19:02:22 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
